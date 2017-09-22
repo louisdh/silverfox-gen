670d6a5563b9d83263b244600046677c02f0bb58
Louis D'hauwe <louisdhauwe@silverfox.be>
2017-09-22 21:18:16 +0200

    Update Swift packages

--- generator/Package.swift
+++ generator/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
 	name: "SilverFox-gen",
 	dependencies: [
-		.Package(url: "https://github.com/vapor-community/markdown.git", majorVersion: 0, minor: 3),
-		.Package(url: "https://github.com/JohnSundell/Files.git", majorVersion: 1, minor: 8)
+		.Package(url: "https://github.com/vapor-community/markdown.git", majorVersion: 0, minor: 4),
+		.Package(url: "https://github.com/JohnSundell/Files.git", majorVersion: 1)
 		]
 )
